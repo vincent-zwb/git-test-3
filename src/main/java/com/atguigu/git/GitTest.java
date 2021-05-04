8c0021bd8372fb1f4b2364046e2244a4b6f25cc4
vincent <215646891@qq.com>
2021-05-04 23:28:36 +0800

    1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,18 +2,6 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args){
-        System.out.println("hello git!");
-        System.out.println("hello git2!");
-        System.out.println("hello git3!");
-        System.out.println("hello git4!");
-        System.out.println("master test!");
-        System.out.println("hot-fix test");
-        System.out.println("push test!!");
-        System.out.println("hahahahahahah");
-        System.out.println("chongtuhebing1+3");
-        System.out.println("chongtuhebing1+2+3");
-        System.out.println("chongtuhebing1+2+4+6");
-        System.out.println("chongtuhebing1+2+4+5+8");
-        System.out.println("chongtuhebing1+2+4+5+7+9");
+        System.out.println("1");
     }
 }
